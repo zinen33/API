20fce7fe2db2ef5796316146cc8aa56a32906205
ZAKI <156739195+zinen33@users.noreply.github.com>
2025-03-31 02:43:05 -0700

    Update kkk.js

--- kkk.js
+++ kkk.js
@@ -2,11 +2,9 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 
-// رابط الصورة
 const imageUrl =
   "https://wxtechhk.oss-cn-hongkong.aliyuncs.com/tasks/output/visual_image_watermarking/e5e7914b-7c7d-4c35-a476-a69b4de685a1.png?x-oss-credential=LTAI5tGjJnh66c1txANiRBQN/20241213/cn-hongkong/oss/aliyun_v4_request&x-oss-date=20241213T092349Z&x-oss-expires=3600&x-oss-signature=138ede21da5e67b2c1771c5c29715fc3353d1a341aa8921ef41be52cd85f6498&x-oss-signature-version=OSS4-HMAC-SHA256";
 
-// مسار حفظ الصورة
 const imagePath = path.join(__dirname, "downloaded_image.png");
 
 (async () => {
@@ -17,7 +15,6 @@ const imagePath = path.join(__dirname, "downloaded_image.png");
       responseType: "stream",
     });
 
-    // كتابة الصورة إلى ملف محلي
     const writer = fs.createWriteStream(imagePath);
     response.data.pipe(writer);
 
